1fc4f1ab7d403ad095861fa921960b96c01c5261
Russell Frank <russ.frank@rutgers.edu>
2012-02-19 12:51:36 -0500

    adding less tests

--- test/general.js
+++ test/general.js
@@ -5,13 +5,67 @@ var vows = require('vows'),
     suite = vows.describe('tylus'),
     exec = require('child_process').exec;
 
+function styleLookupTests (type) {
+   return {
+      class1: {
+         topic: function () {
+            T.setProperty('someprop', 'someval');
+            return T.getProps(type, {tyle: '.class1'});
+         },
+
+         'class property': function (topic) {
+            assert.equal(topic.text, "why hello there");
+         },
+
+         'base property': function (topic) {
+            assert.equal(topic.color, '#00f');
+         },
+
+         'object property': function (topic) {
+            assert.equal(topic.font.fontSize, 15);
+            assert.equal(topic.font.fontStyle, 'bold');
+         },
+
+         'iphone conditional': function (topic) {
+            assert.equal(topic.top, 40);
+         },
+
+         'nested version conditional': function (topic) {
+            assert.equal(topic.left, 27);
+         },
+
+         'nested custom conditional': function (topic) {
+            assert.equal(topic.backgroundColor, '#fefefe');
+         }
+      },
+
+      'someid class2': {
+         topic: function () {
+            return T.getProps(type, {tyle: '#someid .class2', strings: {message: "foo123"}});
+         },
+
+         'class property': function (topic) {
+            assert.equal(topic.right, 34);
+         },
+
+         'string interpolation': function (topic) {
+            assert.equal(topic.text, "hello there foo123");
+         },
+
+         'evaluation': function (topic) {
+            assert.equal(topic.computedProperty, "[object Object] is great");
+         }
+      }
+   };
+}
+
 suite.addBatch({
    tylus: { 
       topic: function () {
          exec('tylus general', this.callback);
       },
 
-      runs: function (err, stderr, stdout) {
+      runs: function (err, stdout, stderr) {
          assert.isNull(err);
       },
 
@@ -25,58 +79,8 @@ suite.addBatch({
          } 
       },
 
-      lookup: {
-         class1: {
-            topic: function () {
-               T.setProperty('someprop', 'someval');
-               return T.getProps('Label', {tyle: '.class1'});
-            },
-
-            'class property': function (topic) {
-               assert.equal(topic.text, "why hello there");
-            },
-
-            'base property': function (topic) {
-               assert.equal(topic.color, '#00f');
-            },
-
-            'object property': function (topic) {
-               assert.equal(topic.font.fontSize, 15);
-               assert.equal(topic.font.fontStyle, 'bold');
-            },
-
-            'iphone conditional': function (topic) {
-               assert.equal(topic.top, 40);
-            },
-
-            'nested version conditional': function (topic) {
-               assert.equal(topic.left, 27);
-            },
-
-            'nested custom conditional': function (topic) {
-               assert.equal(topic.backgroundColor, '#fefefe');
-            }
-         },
-
-         'someid class2': {
-            topic: function () {
-               return T.getProps('Label', {tyle: '#someid .class2', strings: {message: "foo123"}});
-            },
-
-            'class property': function (topic) {
-               assert.equal(topic.right, 34);
-            },
-
-            'string interpolation': function (topic) {
-               assert.equal(topic.text, "hello there foo123");
-            },
-
-            'evaluation': function (topic) {
-               assert.equal(topic.computedProperty, "[object Object] is great");
-            }
-         }
-
-      }
+      stylus: styleLookupTests('Label'),
+      less: styleLookupTests('DashItem')
    }
 });
 
